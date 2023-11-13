2833bd7bcd23ebf5e41627959fe0b4efcb91e1ae
Al Stone <astone@heckgrammar.co.uk>
2023-11-13 12:17:15 +0000

    Add project files.

---
+++ ReadStringIntoStructureArray/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+    Structure Thing
+        Dim ThingName As String
+        Dim ThingAge As Integer
+        Dim ThingRating As Integer
+        Dim ThingScore As Double
+        Sub New(tName, tAge, tRating, tScore)
+            ThingName = tName
+            ThingAge = tAge
+            ThingRating = tRating
+            ThingScore = tScore
+        End Sub
+    End Structure
+    Sub Main()
+        Dim Things(10) As Thing
+        Dim demoThing As String = "Big thing,14,4,12.5"
+        Dim demoSplit = demoThing.Split(",")
+        Things(0) = New Thing(demoSplit(0), demoSplit(1), demoSplit(2), demoSplit(3))
+        Console.WriteLine(Things(0).ThingName)
+        Console.WriteLine(Things(0).ThingAge)
+    End Sub
+
+End Module
---
+++ ReadStringIntoStructureArray/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ReadStringIntoStructureArray")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HGS-SCCM")>
+<Assembly: AssemblyProduct("ReadStringIntoStructureArray")>
+<Assembly: AssemblyCopyright("Copyright © HGS-SCCM 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3a26f584-fa68-4a01-8b05-126867989b20")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
